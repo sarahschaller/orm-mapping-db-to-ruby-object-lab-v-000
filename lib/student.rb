d6134b65e2e370a80f07ce4f5171f97ba920f2fa
Sarah Schaller <sarcsch@aol.com>
2017-09-07 14:36:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,17 @@ class Student
     DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}.first
   end
 
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, 9)
+  end
+
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
